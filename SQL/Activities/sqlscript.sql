8f0ec94f2a1f3bd722fed71336bbb7d162be763e
sudheer10081999 <95345330+sudheer10081999@users.noreply.github.com>
2024-06-23 15:51:06 +0530

    Add files via upload

---
+++ SQL/Activities/sqlscript.sql
@@ -0,0 +1,16 @@
+REM   Script: Session 01
+REM   FST
+
+Describe salesman
+
+
+Describe salesman
+
+
+drop table salesman;
+
+Describe salesman
+
+
+Create table salesman(salesman_id int,salesman_name varchar(20),salesman_city varchar(20),commission int);
+
