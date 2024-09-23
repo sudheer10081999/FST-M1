aaceec5976a97909f751856b6856c56fbc63a284
sudheer10081999 <95345330+sudheer10081999@users.noreply.github.com>
2024-09-23 10:08:33 +0530

    Add files via upload

---
+++ Cucumber/Activities/Activity6_ test-reports.java
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<testsuite name="Cucumber" time="12.067" tests="1" skipped="0" failures="0" errors="0">
+<testcase classname="Testing with Tags" name="Test for Simple Alert" time="6.302">
+<system-out><![CDATA[
+Given User is on the page...................................................passed
+When User clicks the Simple Alert button....................................passed
+Then Alert opens............................................................passed
+And Read the text from it and print it......................................passed
+And Close the alert.........................................................passed
+And Close Browser...........................................................passed
+]]></system-out>
+</testcase>
+</testsuite>
